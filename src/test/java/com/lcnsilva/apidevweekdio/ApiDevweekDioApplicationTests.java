28421f314ba0a827c658d068864cc08665bd9618
Luciano Silva <luciano.qsw@gmail.com>
2023-10-21 00:04:22 -0300

    initial commit

---
+++ src/test/java/com/lcnsilva/apidevweekdio/ApiDevweekDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcnsilva.apidevweekdio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDevweekDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
